60a4b5b5a7b29d04654af3620aacf42eca7b8286
CaptainA10 <167899639+CaptainA10@users.noreply.github.com>
2024-05-21 00:21:52 +0200

    Update testic.py
    update the file about the modifications due to csv file

--- testic.py
+++ testic.py
@@ -1,31 +1,21 @@
 import unittest
-from tic import Board, TicTacToe, HumanPlayerFactory, ComputerPlayerFactory
+from tictactoe import Board, TicTacToe, HumanPlayerFactory
 
 class TestTicTacToe(unittest.TestCase):
 
-    def test_board_display(self):
-        board = Board()
-        expected_display = " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 "
-        self.assertEqual(board.display(), expected_display)
-
     def test_board_update(self):
         board = Board()
-        board.update(1, 'X')
-        self.assertEqual(board.cells[0], 'X')
-
-    def test_board_available_moves(self):
-        board = Board()
-        board.cells = ['X', 'O', '3', '4', '5', '6', '7', '8', '9']
-        self.assertEqual(board.available_moves(), ['3', '4', '5', '6', '7', '8', '9'])
+        board.update(0, 0, 'X')
+        self.assertEqual(board.cells[0][0], 'X')
 
     def test_board_is_winner(self):
         board = Board()
-        board.cells = ['X', 'X', 'X', '4', '5', '6', '7', '8', '9']
+        board.cells = [['X', 'X', 'X'], ['', '', ''], ['', '', '']]
         self.assertTrue(board.is_winner('X'))
 
     def test_board_is_full(self):
         board = Board()
-        board.cells = ['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O']
+        board.cells = [['X', 'O', 'X'], ['O', 'X', 'O'], ['O', 'X', 'O']]
         self.assertTrue(board.is_full())
 
     def test_on_button_click(self):
@@ -41,7 +31,7 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_validate_move(self):
         game = TicTacToe(HumanPlayerFactory())
-        @game.validate_move
+        @validate_move
         def mock_on_button_click(self, i, j):
             return "Clicked"
         result = mock_on_button_click(game, 0, 0)
@@ -49,7 +39,7 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_show_game_over_message(self):
         game = TicTacToe(HumanPlayerFactory())
-        @game.show_game_over_message
+        @show_game_over_message
         def mock_game_over(self, message):
             return "Game Over"
         result = mock_game_over(game, "Player X wins!")
@@ -57,3 +47,4 @@ class TestTicTacToe(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
